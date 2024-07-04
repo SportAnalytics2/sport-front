7aaadba7704549a08bf036f36f400c5d81b0d5a2
santiago-federici <santiagofederici88@gmail.com>
2024-07-04 11:28:16 -0300

    update: added theme toggle to the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from "./theme-toggle";
 import Wrapper from "./wrapper";
 
 export default function Navbar() {
@@ -6,11 +7,15 @@ export default function Navbar() {
       <Wrapper>
         <nav className="flex w-full items-center justify-between">
           <h1 className="text-2xl">Club San Martin</h1>
-          <img
-            src="https://randomuser.me/api/portraits/men/3.jpg"
-            alt="profile picture"
-            className="size-10 rounded-full"
-          />
+
+          <div className="flex items-center gap-2">
+            <ThemeToggle />
+            <img
+              src="https://randomuser.me/api/portraits/men/3.jpg"
+              alt="profile picture"
+              className="size-10 rounded-full"
+            />
+          </div>
         </nav>
       </Wrapper>
     </header>
