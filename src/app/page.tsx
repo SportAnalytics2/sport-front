355165807f6920ff08f11505cb46ac35c514dd3d
santiago-federici <santiagofederici88@gmail.com>
2024-07-05 16:33:11 -0300

    update: added some text to the temporary first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
 
+import Wrapper from "@/components/wrapper";
+import { buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <Link href={"/disciplines"}>Ir a disciplinas</Link>
+      <Wrapper>
+        <p>Aqui iria probablemente un login</p>
+        <Link
+          href={"/disciplines"}
+          className={buttonVariants({ variant: "secondary" })}
+        >
+          Ir a disciplinas
+        </Link>
+      </Wrapper>
     </main>
   );
 }
